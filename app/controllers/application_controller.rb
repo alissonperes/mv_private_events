840ba4b4e4e49be5c6518a08f33066e30a388729
David Elì <davidelimg1@gmail.com>
2020-01-16 10:27:11 -0600

    Removed methiods from helpers and added logic into controller like the changes required

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
-  include SessionsHelper
+  before_action :current_user
+
+  def current_user
+    return nil unless session[:user_id]
+
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def current_user=(user)
+    if user
+      session[:user_id] = user.id
+    else
+      session.delete :user_id
+    end
+    @current_user = user || nil
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
 end
