20e8d0d1ca312adf2f8e258fa6734d099eaab0db
Alisson Peres <alissonpres.dll@gmail.com>
2020-01-16 19:45:52 -0300

    Fixed linter errors

--- spec/models/attendance_models_spec.rb
+++ spec/models/attendance_models_spec.rb
@@ -3,16 +3,22 @@
 require 'rails_helper'
 
 RSpec.describe Attendance, type: :model do
-  let(:user) { User.create!(name: 'Foobar', email: 'foobar@bar.com',
-                            password: 'foobar', password_confirmation: 'foobar') }
-  let(:user2) { User.create!(name: 'Foobar2', email: 'foobar2@bar.com',
-                            password: 'foobax', password_confirmation: 'foobax') }
+  let(:user) do
+    User.create!(name: 'Foobar', email: 'foobar@bar.com',
+                 password: 'foobar', password_confirmation: 'foobar')
+  end
+
+  let(:user2) do
+    User.create!(name: 'Foobar2', email: 'foobar2@bar.com',
+                 password: 'foobax', password_confirmation: 'foobax')
+  end
+
   let(:event) do
     user.events.create!(name: 'Anything', description: 'Anything',
                         location: 'Anywhere', scheduled: Time.now + 1.month)
   end
 
-  subject { Attendance.new  }
+  subject { Attendance.new }
 
   it 'is valid with valid attributes' do
     subject.event_id = event.id
